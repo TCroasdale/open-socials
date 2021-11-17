a29eaf82347389890d65dc8027180d49d147d9fe
Tom Croasdale <TCroasdale@users.noreply.github.com>
2021-11-17 16:35:29 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # open-socials
  An open source social media management tool
+
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+const electron = window.require("electron")
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  mounted: function(){
+    electron.ipcRenderer.on('goToHome', ()=>{
+      this.$router.push('/');
+    });
+    electron.ipcRenderer.on('goToAbout', ()=>{
+      this.$router.push('/about');
+    });
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
